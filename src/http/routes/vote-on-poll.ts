0adb65cbca63e28e0ba4e3025c7da933a47efcfa
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-02-09 17:49:46 -0300

    Aula 02 concluida

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,64 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+import { randomUUID } from "node:crypto"
+import { prisma } from "../../lib/prisma";
+
+
+export async function VoteOnPoll(app: FastifyInstance) {
+    app.post('/polls/:pollId/voltes', async (request, reply) => {
+        const VoteOnPollBody = z.object({
+            pollOptionId: z.string().uuid()
+        })
+
+        const VoteOnPollParams = z.object({
+            pollId: z.string().uuid()
+        })
+
+        const { pollId } = VoteOnPollParams.parse(request.params)
+        const { pollOptionId } = VoteOnPollBody.parse(request.body)
+
+        let { sessionId: sessionID } = request.cookies
+
+        if (sessionID) {
+            const userVotePoll = await prisma.vote.findUnique({
+                where: {
+                    sessionID_pollId: {
+                        sessionID,
+                        pollId
+                    }
+                }
+            })
+            if (userVotePoll && userVotePoll.pollOptionId != pollOptionId) {
+                await prisma.vote.delete({
+                    where: {
+                        id: userVotePoll.id
+                    }
+                })
+            } else if (userVotePoll) {
+                return reply.status(400).send({ message: "voce ja votou nesta enquete" })
+            }
+
+        }
+
+        if (!sessionID) {
+            sessionID = randomUUID()
+
+            reply.setCookie('sessionId', sessionID, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 30,
+                signed: true,
+                httpOnly: true
+            })
+        }
+
+        await prisma.vote.create({
+            data: {
+                sessionID,
+                pollId,
+                pollOptionId,
+            }
+        })
+
+        return reply.status(201).send()
+    })
+}
